10d8761bb1d94a9f4f6c08b8896059f25432f71c
Maija Kekkonen <mkkekkonen@protonmail.com>
2018-12-20 22:55:05 +0200

    Add constant acceleration page
    + improve vector construction
    + fix mobile styles

---
+++ static/mathvisualized/es6/src/entry_points/mechanics/kinematics/constaccel.js
@@ -0,0 +1,77 @@
+import Konva from 'konva';
+import * as util from '../../../util/util';
+import * as axis2DRenderer from '../../../renderers/axis2DRenderer';
+import * as dotRenderer from '../../../renderers/dotRenderer';
+import Vector3 from '../../../math/vector';
+import TimeKinematics2D from '../../../physics/timeKinematics2D';
+
+const stage = util.getDefaultKonvaStage();
+const layer = new Konva.Layer();
+stage.add(layer);
+
+axis2DRenderer.addAxesToLayer(layer);
+layer.draw();
+
+let running = false;
+
+const initialPosition = new Vector3();
+const initialVelocity = new Vector3();
+const acceleration = new Vector3();
+
+const dot = {
+    kinematics: new TimeKinematics2D({ initialPosition, initialVelocity, acceleration }),
+};
+
+const start = () => {
+    const initialVelocityX = parseFloat(document.getElementById('v0x').value) || 0;
+    const initialVelocityY = parseFloat(document.getElementById('v0y').value) || 0;
+    const _initialVelocity = new Vector3({ x: initialVelocityX, y: initialVelocityY });
+
+    const accelerationX = parseFloat(document.getElementById('ax').value) || 0;
+    const accelerationY = parseFloat(document.getElementById('ay').value) || 0;
+    const _acceleration = new Vector3({ x: accelerationX, y: accelerationY });
+
+    dot.kinematics.reset({
+        initialPosition,
+        initialVelocity: _initialVelocity,
+        acceleration: _acceleration,
+    });
+
+    running = true;
+};
+
+const reset = () => {
+    running = false;
+    dot.kinematics.reset({ initialPosition, initialVelocity, acceleration });
+};
+
+document.getElementById('startButton').addEventListener('click', start);
+document.getElementById('resetButton').addEventListener('click', reset);
+
+const update = (time) => {
+    dot.kinematics.update(time);
+};
+
+const render = () => {
+    layer.removeChildren();
+    axis2DRenderer.addAxesToLayer(layer);
+    dotRenderer.addDotToLayer({
+        point: dot.kinematics.position,
+        layer,
+    });
+    layer.draw();
+    document.getElementById('output').innerHTML = dot.kinematics.toString();
+};
+
+let previousTime = new Date().getTime();
+
+window.setInterval(() => {
+    const currentTime = new Date().getTime();
+    const timeDelta = currentTime - previousTime;
+    const timeDeltaSeconds = timeDelta / 1000;
+    if (running) {
+        update(timeDeltaSeconds);
+    }
+    render();
+    previousTime = currentTime;
+}, 1000.0 / 60.0);
---
+++ static/mathvisualized/es6/src/physics/timeKinematics2D.js
@@ -0,0 +1,46 @@
+import Vector3 from '../math/vector';
+import { round } from '../math/util';
+
+class TimeKinematics2D {
+    constructor({ initialPosition, initialVelocity, acceleration }) {
+        this.reset({ initialPosition, initialVelocity, acceleration });
+    }
+
+    reset({ initialPosition, initialVelocity, acceleration }) {
+        this.initialPosition = initialPosition;
+        this.position = initialPosition || new Vector3({ x: 0, y: 0, z: 0 });
+
+        this.initialVelocity = initialVelocity;
+        this.velocity = initialVelocity;
+
+        this.acceleration = acceleration;
+
+        this.totalTime = 0;
+    }
+
+    update(time) {
+        this.totalTime += time;
+
+        const x = (this.initialVelocity.x * this.totalTime)
+            + (0.5 * this.acceleration.x * (this.totalTime ** 2));
+        const y = (this.initialVelocity.y * this.totalTime)
+            + (0.5 * this.acceleration.y * (this.totalTime ** 2));
+        this.position = new Vector3({ x, y });
+
+        const vx = this.initialVelocity.x
+            + (this.acceleration.x * this.totalTime);
+        const vy = this.initialVelocity.y
+            + (this.acceleration.y * this.totalTime);
+        this.velocity = new Vector3({ x: vx, y: vy });
+    }
+
+    toString() {
+        let str = 'TimeKinematics2D\n';
+        str += `~ position: (${round(this.position.x)}, ${round(this.position.y)})\n`;
+        str += `~ velocity: (${round(this.velocity.x)}, ${round(this.velocity.y)})\n`;
+        str += `~ acceleration: (${round(this.acceleration.x)}, ${round(this.acceleration.y)})`;
+        return str;
+    }
+}
+
+export default TimeKinematics2D;
